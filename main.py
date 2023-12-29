0b1616352f1e5101cc24361e5ce419e419ecdab8
IgnacyBerent <2gb02ignac@gmail.com>
2023-12-29 22:55:25 +0100

    Adjusting code for firebase data storage

--- main.py
+++ main.py
@@ -16,7 +16,18 @@ app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 Bootstrap5(app)
 
 # Initialize Firebase
-cred = credentials.Certificate("credentials.json")
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": os.environ.get('PROJECT_ID'),
+    "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
+    "private_key": os.environ.get('PRIVATE_KEY'),
+    "client_email": os.environ.get('CLIENT_EMAIL'),
+    "client_id": os.environ.get('CLIENT_ID'),
+    "auth_uri": os.environ.get('AUTH_URI'),
+    "token_uri": os.environ.get('TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.environ.get('AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.environ.get('CLIENT_X509_CERT_URL')
+})
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://bioaddmedknowledgebase-default-rtdb.europe-west1.firebasedatabase.app/'})
 ref = db.reference('/')
